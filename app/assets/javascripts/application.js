28811ad69fbf09a8236aaa30aead81d292a31336
kyle blackmore <blackmorekwb@gmail.com>
2015-04-16 13:23:47 -0700

    js checkpoint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,58 @@ $(document).ready(function() {
     $(this).tab('show');
   });
 
+  $('#pg-btn').click(function(){
+    var div = document.getElementById('pg-well');
+    changeBgColor(div, '#F0F');
+
+
+  });
+
+  $('#contact-btn').hover(function() {
+    $(this).addClass("pulse");
+  });
+  
+
+  function changeBgColor (div, color) {
+
+    //var elem = document.getElementById(div);
+    div.style.transition = "background 2.0s ease-in 0s";
+    div.style.background = color;
+
+
+  }
+
+/*
+  $(window).scroll(function() {
+    $('#animatedElement').each(function(){
+      var imagePos    = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+350) {
+        //$(this).addClass("slideLeft");
+        changeBgColor($(this), '#F0F');
+      }
+    });
+  });
+*/
+  /* CSS animations */
+  /*
+  //element.style.webkitAnimationPlayState = "paused";
+  //element.style.webkitAnimationPlayState = "running";
+
+  $('#contact-btn').hover(function() {
+    $(this).addClass("pulse");
+  });
+
+  $(window).scroll(function() {
+    $('#animatedElement').each(function(){
+      var imagePos    = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+400) {
+        $(this).addClass("slideLeft");
+      }
+    });
+  });
+*/
+
+
 });
